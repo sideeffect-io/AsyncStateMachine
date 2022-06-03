05469ec8f2a2806a1d8d853c6046d1035be01940
Thibault Wittemberg <thibault.wittemberg@gmail.com>
2022-06-03 14:54:40 +0200

    declare Package

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.5
+
+import PackageDescription
+
+let package = Package(
+    name: "AsyncStateMachine",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6)
+    ],
+    products: [
+        .library(
+            name: "AsyncStateMachine",
+            targets: ["AsyncStateMachine"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "AsyncStateMachine",
+            dependencies: [],
+            path: "Sources/AsyncStateMachine"),
+        .testTarget(
+            name: "AsyncStateMachineTests",
+            dependencies: ["AsyncStateMachine"],
+            path: "Tests/AsyncStateMachine"),
+    ]
+)
